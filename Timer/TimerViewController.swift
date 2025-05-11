6bea78d6ae30a0a3188c03392f1e75ef097c1be8
hikotann33 <Japaninternationalairline@familybright.org>
2025-05-11 15:02:33 +0800

    最終

--- Timer/TimerViewController.swift
+++ Timer/TimerViewController.swift
@@ -8,33 +8,70 @@
 import UIKit
 
 class TimerViewController: UIViewController {
+    var timer: Timer!
+    var countdown: Int = 0
     var timeViewHour : String?
     var timeViewmini : String?
     var timeSubject: String?
     
-    @IBOutlet weak var timeViewHourLabel: UILabel!
-    @IBOutlet weak var timeViewminiLabel: UILabel!
-    @IBOutlet weak var timeViewsubject: UILabel!
+    func startTimer(hour: Int, minute: Int) {
+        
+        // もしタイマーが動いていたら止める
+        if timer != nil {
+            timer.invalidate()
+        }
+        let time: Int = hour * 3600 + minute * 60
+        countdown = time
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
+    }
     
+    /// タイマーによって1分ごとに呼ばれる関数
+    @objc func onTimerCalled() {
+        updateLabel()
+        updateCountdown()
+        
+        if isTimeOver() {
+            finishTimer()
+        }
+    }
+    
+    /// ラベルを更新する
+    func updateLabel() {
+        let remainingminiture: Int = countdown % 60
+        let remainingMinutes: Int = countdown / 60
+        let remainingSeconds: Int = countdown % 60
+        timelabel.text = String(format: "%02d:%02d:%02d", remainingminiture,remainingMinutes, remainingSeconds)
+    }
+    
+    /// 残り時間を更新する
+    func updateCountdown() {
+        countdown -= 1
+    }
+    
+    /// 残り時間が0かどうか判定する
+    /// - Returns: true: 0になった, false: 0より大きい
+    func isTimeOver() -> Bool {
+        return countdown < 0
+    }
+    
+    /// タイマー終了時の処理
+    func finishTimer() {
+        timer.invalidate()
+        
+        let stopAlert = UIAlertController(title: "タイマーが終了しました", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        stopAlert.addAction(okAction)
+        present(stopAlert, animated: true)
+    }
+    @IBOutlet weak var timelabel: UILabel!
+    @IBOutlet weak var timeViewsubject: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let minute = Int(timeViewmini ?? "0") ?? 0
+        let hour = Int(timeViewHour ?? "0") ?? 0
+        print(timeViewHour ,"hour")
         
-        print(timeViewHour, "bbbbbb")
-        timeViewHourLabel.text = timeViewHour
-        timeViewminiLabel.text = timeViewmini
+        startTimer(hour: hour, minute: minute)
         timeViewsubject.text = timeSubject
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -2,11 +2,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var label: UILabel!
-    
-    var timer: Timer!
-    var countdown: Int = 0
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -15,88 +10,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var minuteTextField: UITextField!
     @IBOutlet weak var subjectTextField: UITextField!
     @IBOutlet weak var startButton: UIButton!
-    
-    //MARK: -  タイマーの処理
-    
-    /// タイマーを開始する関数
-    /// - Parameter time: 設定する時間（秒）
-    func startTimer(time: Int) {
-        // もしタイマーが動いていたら止める
-        if timer != nil {
-            timer.invalidate()
-        }
-        
-        countdown = time
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerCalled), userInfo: nil, repeats: true)
-    }
-    
-    /// タイマーによって1秒ごとに呼ばれる関数
-    @objc func onTimerCalled() {
-        updateLabel()
-        updateCountdown()
-        
-        if isTimeOver() {
-            finishTimer()
-        }
-    }
-    
-    /// ラベルを更新する
-    func updateLabel() {
-        let remainingMinutes: Int = countdown / 60
-        let remainingSeconds: Int = countdown % 60
-        label.text = String(format: "%02d:%02d", remainingMinutes, remainingSeconds)
-    }
-    
-    /// 残り時間を更新する
-    func updateCountdown() {
-        countdown -= 1
-    }
-    
-    /// 残り時間が0かどうか判定する
-    /// - Returns: true: 0になった, false: 0より大きい
-    func isTimeOver() -> Bool {
-        return countdown < 0
-    }
-    
-    /// タイマー終了時の処理
-    func finishTimer() {
-        timer.invalidate()
-        
-        let stopAlert = UIAlertController(title: "タイマーが終了しました", message: nil, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        stopAlert.addAction(okAction)
-        present(stopAlert, animated: true)
-    }
 
-    // MARK: - ボタンの処理
-//    test233
-    
-    @IBAction func select30seconds() {
-        startTimer(time: 30)
-    }
 
-    @IBAction func select1minute() {
-        startTimer(time: 60)
-    }
-    
-    @IBAction func select5minutes() {
-        startTimer(time: 300)
-    }
-    
-    @IBAction func select10minutes() {
-        startTimer(time: 600)
-    }
-    @IBAction func selectcustom(){
-        let time = Int(inputview.text ?? "0") ?? 0
-        startTimer(time:time)
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             // ②Segueの識別子確認
             if segue.identifier == "totimerview" {
                 // ③遷移先ViewCntrollerの取得
                 let timerViewController = segue.destination as! TimerViewController
                 // ④値の設定
-                print(hourTextField.text, "aaaaa")
                 timerViewController.timeViewHour = hourTextField.text
                 timerViewController.timeViewmini = minuteTextField.text
                 timerViewController.timeSubject = subjectTextField.text
